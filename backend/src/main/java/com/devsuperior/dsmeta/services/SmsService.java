0e1617d4136643a139f8cb0b6ed8ae3bc59198da
Reginaldo B. F <reginaldobf21@gmail.com>
2022-11-20 22:53:37 -0300

    Send notification

--- backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
@@ -33,7 +33,8 @@ public class SmsService {
 		Sale sale = saleRepository.findById(saleId).get();
 		
 		String date = sale.getDate().getMonthValue() + "/" + sale.getDate().getYear();
-		String msg = "O Vendedor " + sale.getSellerName() + " foi destaque em " + date + " com um total de R$ " + String.format("%.2f", sale.getAmount()); 
+		//String msg = "O Vendedor " + sale.getSellerName() + " foi destaque em " + date + " com um total de R$ " + String.format("%.2f", sale.getAmount()); 
+		String msg = "O Vendedor  foi destaque em ";
 		
 		Twilio.init(twilioSid, twilioKey);
 
